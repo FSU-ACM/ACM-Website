10e4c8d9c2d962bbce6b3200a0dca8abc85bd451
andrewsosa001 <andrewsosa001@gmail.com>
2017-01-30 16:51:49 -0500

    ugh updates

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_flatpages import FlatPages
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+
+# Init
+app = Flask(__name__)
+pages = FlatPages(app)
+bootstrap = Bootstrap(app)
+
+
+# Config
+app.config['FLATPAGES_AUTO_RELOAD'] = True
+app.config['FLATPAGES_EXTENSION'] = '.md'
+app.config['FREEZER_RELATIVE_URLS'] = True
+# app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html; charset=utf-8'
+app.url_map.strict_slashes = False
+
+
+# Nav
+topbar = Navbar('ACM at FSU',
+    Link('Home', '/'),
+    Link('About', '/about'),
+    Link('Sponsors', '/sponsors'),
+    Link('Contest', 'http://springprogrammingcontest.com'),
+    Subgroup('Links',
+        Link('Slack', 'https://acmatfsu.slack.com'),
+        Link('Facebook', 'https://www.facebook.com/groups/cs.fsu.acm/'),
+        Link('Nole Central', 'https://nolecentral.dsa.fsu.edu/organization/associationforcomputingmachinery'),
+    )
+)
+
+nav = Nav()
+nav.register_element('top', topbar)
+nav.init_app(app)
+
+from views import *
