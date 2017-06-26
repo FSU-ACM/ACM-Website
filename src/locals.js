5de47bf6eedf0134c786549e5092d341bb94ce99
Unknown <andrewsosa001@gmail.com>
2017-06-26 16:58:20 -0400

    Add events reference to locals.js

--- src/locals.js
+++ src/locals.js
@@ -1,7 +1,9 @@
 // locals.js
 var locals = {};
 
-locals.css = "./css"
-locals.img = "./assets/img"
+locals.css = "./css";
+locals.img = "./assets/img";
+
+locals.events = require("./events.json").events;
 
 module.exports = locals;
