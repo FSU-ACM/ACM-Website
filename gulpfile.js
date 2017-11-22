27115615c184448ce6ebec798a82c8a52e181afd
Andrew Sosa <andrewsosa001@gmail.com>
2017-11-21 23:24:34 -0500

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,64 @@
-const path = require('path')
-
-const gulp    = require('gulp');
+const gulp    = require('gulp')
 const flatten = require('gulp-flatten')
 
-const source = 'src';
-const assets = 'assets';
-const build = 'build';
-const deploy = 'html';
+const build = 'build'
 
 gulp.task('pug', function() {
-    const pug    = require('gulp-pug');
-    const locals = require('./src/locals.js');
+    const pug    = require('gulp-pug')
+    const locals = require('./src/locals.js')
 
     gulp.src(['src/views/*/*.pug', '!src/views/_includes/**'])
         .pipe(pug({locals: locals}))
         .on('error', console.log)
         .pipe(flatten())
-        .pipe(gulp.dest(build));
-});
+        .pipe(gulp.dest(build))
+})
 
 gulp.task('sass', function() {
-    const sass = require('gulp-sass');
-    const paths = ['src/styles/'];
+    const sass = require('gulp-sass')
+    const paths = ['src/styles/']
     gulp.src('src/views/**/*.sass')
         .pipe(sass({includePaths: paths})
             .on('error', sass.logError))
         .pipe(flatten())
-        .pipe(gulp.dest(build + '/public/css'));
-});
+        .pipe(gulp.dest(build + '/public/css'))
+})
 
 gulp.task('babel', function() {
-    const babel = require('gulp-babel');
+    const babel = require('gulp-babel')
     return gulp.src('src/js/*.js')
         .pipe(babel())
-        .pipe(gulp.dest(build + '/public/js'));
-});
+        .pipe(gulp.dest(build + '/public/js'))
+})
 
 gulp.task('favicon', function() {
     gulp.src('public/favicon.png')
-        .pipe(gulp.dest(build));
+        .pipe(gulp.dest(build))
 })
 
 gulp.task('assets', ['favicon'], function() {
     gulp.src('assets/img/**')
         .pipe(flatten())
-        .pipe(gulp.dest(build + '/public/img'));
-});
+        .pipe(gulp.dest(build + '/public/img'))
+})
 
 gulp.task('clean', function() {
-    var clean = require('gulp-clean');
+    var clean = require('gulp-clean')
     gulp.src(build + '/**', {read:false})
-        .pipe(clean());
-});
+        .pipe(clean())
+})
 
 gulp.task('watch', ['build'], function() {
-    gulp.watch('src/views/**/*.pug', ['pug']);
-    gulp.watch('src/**/events.json', ['pug']);
-    gulp.watch('src/**/sponsors.json', ['pug']);
-    gulp.watch('src/**/partners.json', ['pug']);
-    gulp.watch('src/views/**/*.sass', ['sass']);
-    gulp.watch('src/styles/*.sass', ['sass']);
-    gulp.watch('public/**', ['assets']);
+    gulp.watch('src/views/**/*.pug', ['pug'])
+    gulp.watch('src/**/events.json', ['pug'])
+    gulp.watch('src/**/sponsors.json', ['pug'])
+    gulp.watch('src/**/partners.json', ['pug'])
+    gulp.watch('src/views/**/*.sass', ['sass'])
+    gulp.watch('src/styles/*.sass', ['sass'])
+    gulp.watch('public/**', ['assets'])
     gulp.watch('src/locals.js', ['pug'])
-});
+})
 
-gulp.task('views', ['pug', 'sass']);
-gulp.task('build', ['views', 'assets', 'babel']);
-gulp.task('default', ['build']);
+gulp.task('views', ['pug', 'sass'])
+gulp.task('build', ['views', 'assets', 'babel'])
+gulp.task('default', ['build'])
